00dc1ddbf43954715d0c5314b945dc77c1fac2de
Jacherr <jwc2002@outlook.com>
2020-11-30 18:40:50 +0100

    ab

--- dist/src/rest/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RatelimitHeaders;
-(function (RatelimitHeaders) {
-    RatelimitHeaders["LIMIT"] = "x-ratelimit-limit";
-    RatelimitHeaders["REMAINING"] = "x-ratelimit-remaining";
-    RatelimitHeaders["RESET"] = "x-ratelimit-reset";
-})(RatelimitHeaders = exports.RatelimitHeaders || (exports.RatelimitHeaders = {}));
