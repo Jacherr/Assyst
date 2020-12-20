c5cc73159b5eb150057e22b19a5cc7d050f6ade9
Timo <30553356+y21@users.noreply.github.com>
2020-12-21 00:19:58 +0100

    biglink command

---
+++ src/commands/util/biglink.ts
@@ -0,0 +1,44 @@
+import { BaseCommand } from '../basecommand';
+import { Context } from 'detritus-client/lib/command';
+import { Message } from 'detritus-client/structures';
+
+interface CommandArgs {
+    link: string
+}
+
+function getProxyUrlFromEmbed(message: Message) {
+  return message.embeds.first().image.proxyUrl;
+}
+
+function generateBigLink(context: Context, url: string): Promise<string> {
+  const initial = await context.reply({ embed: { image: { url } } });
+  
+  let previous: string = getProxyUrlFromEmbed(initial);
+  
+  while (true) {
+    const next = await initial.edit({ embed: { image: { url: previous } } });
+    
+    const url = getProxyUrlFromEmbed(next);
+    if (url.length > 2000) break;
+    previous = url;
+  }
+  
+  return previous;
+}
+
+export default class BiglinkCommand extends BaseCommand {
+    name = 'biglink'
+
+    label = 'link'
+
+    metadata = {
+      description: 'generates a big link',
+      examples: ['https://link.to.my/image.png'],
+      usage: '<attachment|link>'
+    }
+
+    async run (context: Context, args: CommandArgs) {
+        const link = await generateBigLink(args.link);
+        return context.editOrReply(link);
+    }
+}
