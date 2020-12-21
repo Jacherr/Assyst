acfc0fab4b3fd976ffb8392df90569eea8b14010
Jacher <jwc2002@outlook.com>
2020-12-21 16:03:15 +0000

    fix biglink breaking on supplying large link to it

--- src/commands/util/biglink.ts
+++ src/commands/util/biglink.ts
@@ -16,9 +16,15 @@ async function generateBigLink(context: Context, url: string): Promise<string> {
   
   let previous: string = getProxyUrlFromEmbed(initial), counter = 0;
   
+  if(previous.length > 2000) {
+    await initial.delete();
+    return url;
+  }
+
   // We use a counter variable to ensure it never edits more than 10 times
   // This is to prevent infinite loops (in case it somehow never exceeds 2000 characters)
   while (counter++ < 25) {
+    await new Promise((r) => setTimeout(r, 5000));
     const next = await initial.edit({ embed: { description: previous.length + '/2000 characters', image: { url: previous } } });
     
     const url = getProxyUrlFromEmbed(next);
