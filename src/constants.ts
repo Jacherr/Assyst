ba6e8ca6759c3fc15224dccd69baaa7412461b24
Jacher <jwc2002@outlook.com>
2021-01-27 10:38:33 +0000

    delete EVERYTHING

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-export enum EmbedColors {
-    ERROR = 0xb51914,
-    INFO = 0xe04119,
-    ZX8 = 0x0fbcf9
-}
-
-export const EMOJI_REGEX = /<a?:(\w+):\d+>/g;
-export const USER_ID = /<@!?(\d+)>/g;
--- src/constants/badtranslator.ts
+++
@@ -1,8 +0,0 @@
-// Defines how often messages are translated by the same user
-export const TRANSLATION_RATELIMIT_RESET = 5000;
-// Defines the maximum number of users to have their rate limit cached
-export const MAX_CACHE_SIZE = 10;
-// Defines the maximum number of characters a message can have
-export const MAX_MESSAGE_LENGTH = 500;
-// Defines the message content that is sent when someone is sending messages too quickly
-export const RATELIMIT_MESSAGE = ' you\'re sending messages too quickly!';
