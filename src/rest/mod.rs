bf85a85d2d4aff09043d0fc3aac5dc6143463b4f
y21 <btimo8822@gmail.com>
2021-04-01 15:26:21 +0200

    dont leak network errors

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -11,13 +11,35 @@ mod routes {
     pub const OCR: &str = "http://ocr.y21_.repl.co/?url=";
 }
 
-pub async fn ocr_image(client: &Client, url: &str) -> Result<String, Error> {
-    client
+pub enum OcrError {
+    NetworkError,
+    HtmlResponse
+}
+
+impl ToString for OcrError {
+    fn to_string(&self) -> String {
+        match self {
+            Self::NetworkError => "An unknown network error occurred".to_string(),
+            Self::HtmlResponse => "Failed to parse response".to_string()
+        }
+    }
+}
+
+pub async fn ocr_image(client: &Client, url: &str) -> Result<String, OcrError> {
+    let text = client
         .get(&format!("{}{}", routes::OCR, url))
         .send()
-        .await?
+        .await
+        .map_err(|_| OcrError::NetworkError)?
         .text()
         .await
+        .map_err(|_| OcrError::NetworkError)?;
+    
+    if text.to_ascii_lowercase().starts_with("<!doctype html>") {
+        return Err(OcrError::HtmlResponse);
+    }
+
+    return Ok(text)
 }
 
 pub async fn upload_to_filer(
