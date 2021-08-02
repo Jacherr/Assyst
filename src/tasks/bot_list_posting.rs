8c0722cacffa4faa73e4a825be7b09e45d266bb3
Jacherr <jwc2002@outlook.com>
2021-08-02 14:39:44 +0100

    finalise bot list stats [osting

---
+++ src/tasks/bot_list_posting.rs
@@ -0,0 +1,40 @@
+use std::{sync::Arc, time::Duration};
+
+use crate::{assyst::Assyst, rest::{maryjane::get_application, post_bot_stats}};
+
+use tokio::time::sleep;
+
+const FETCH_INTERVAL: u64 = 60000 * 60; // 1 hour
+
+pub fn init_bot_list_posting_loop(assyst: Arc<Assyst>) {
+    tokio::spawn(async move {
+        loop {
+            let guild_count = get_application(assyst.clone(), assyst.config.bot_id)
+                .await
+                .unwrap()
+                .bot
+                .guild_count;
+
+            let result = post_bot_stats(
+                &assyst.reqwest_client,
+                &assyst.config.auth.discord_bot_list_post_stats,
+                &assyst.config.auth.top_gg_post_stats,
+                guild_count as u32,
+            )
+            .await;
+
+            match result {
+                Ok(()) => {},
+                Err(e) => {
+                    assyst.logger.fatal(
+                        assyst.clone(),
+                        &format!("Error POSTing bot list stats: {}", e.to_string()),
+                    )
+                    .await;
+                }
+            }
+
+            sleep(Duration::from_millis(FETCH_INTERVAL)).await;
+        }
+    });
+}
