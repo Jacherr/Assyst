8619269ad774b507bec892eeaee7f3281c96563c
Timo <30553356+y21@users.noreply.github.com>
2021-08-12 21:49:47 +0200

    fix tenor regex

--- src/util.rs
+++ src/util.rs
@@ -30,7 +30,7 @@ pub mod regexes {
 
     lazy_static! {
         pub static ref CUSTOM_EMOJI: Regex = Regex::new(r"<a?:(\w+):(\d{16,20})>").unwrap();
-        pub static ref TENOR_GIF: Regex = Regex::new(r"https://\w+\.tenor\.com/\w+/[^\.]+\.gif").unwrap();
+        pub static ref TENOR_GIF: Regex = Regex::new(r"https://\w+\.tenor\.com/[\w\-]+/[^\.]+\.gif").unwrap();
         pub static ref URL: Regex = Regex::new(r"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)").unwrap();
         pub static ref USER_MENTION: Regex = Regex::new(r"(?:<@!?)?(\d{16,20})>?").unwrap();
         pub static ref TIME_STRING: Regex = Regex::new("(\\d+)([smhd])").unwrap();
