d3448772881b9e8b81181ff65dc2788b0cdb08fd
Jacherr <jwc2002@outlook.com>
2022-06-24 11:44:40 +0100

    Revert "assyst-gateway unfinished state"
    This reverts commit caf7d86d644701b82196ae5ee46b39bc679e774b.

--- assyst-bot/src/main.rs
+++ assyst-bot/src/main.rs
@@ -14,14 +14,20 @@ mod rest;
 mod tasks;
 mod util;
 
+use anyhow::Context;
 use assyst::Assyst;
-use assyst_common::consts::EVENT_PIPE;
 use assyst_webserver::run as webserver_run;
+use dotenv::dotenv;
+use futures::stream::StreamExt;
 use handler::handle_event;
-use serde::de::DeserializeSeed;
-use tokio::{io::AsyncReadExt, net::{UnixStream, UnixListener}};
-use twilight_model::gateway::event::GatewayEventDeserializer;
+use std::env;
 use std::sync::Arc;
+use twilight_gateway::cluster::Cluster;
+use twilight_model::gateway::payload::outgoing::update_presence::UpdatePresencePayload;
+use twilight_model::gateway::{
+    presence::{Activity, ActivityType, Status},
+    Intents,
+};
 
 #[cfg(target_os = "linux")]
 #[global_allocator]
@@ -29,7 +35,53 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let assyst = Arc::new(Assyst::new().await);
+    dotenv().ok();
+    let token = env::var("DISCORD_TOKEN")?;
+
+    let assyst = Arc::new(Assyst::new(&token).await);
+    let activity = Activity {
+        application_id: None,
+        assets: None,
+        created_at: None,
+        details: None,
+        emoji: None,
+        flags: None,
+        id: None,
+        instance: None,
+        kind: ActivityType::Playing,
+        name: format!("{}help | jacher.io/assyst", assyst.config.prefix.default),
+        party: None,
+        secrets: None,
+        state: None,
+        timestamps: None,
+        url: None,
+        buttons: Vec::new(),
+    };
+    let presence = UpdatePresencePayload::new(vec![activity], false, None, Status::Online)?;
+
+    // spawn as many shards as discord recommends
+    let (cluster, mut events) = Cluster::builder(
+        token,
+        Intents::MESSAGE_CONTENT | Intents::GUILD_MESSAGES | Intents::GUILDS,
+    )
+    .http_client(assyst.http.clone())
+    .presence(presence)
+    .build()
+    .await?;
+
+    let arced_cluster = Arc::new(cluster);
+
+    let spawned_cluster = arced_cluster.clone();
+    let a = assyst.clone();
+    tokio::spawn(async move {
+        spawned_cluster.up().await;
+        let cluster_info = spawned_cluster.info();
+        let mut text = String::new();
+        for shard in cluster_info {
+            text.push_str(&format!("Shard {} is {}\n", shard.0, shard.1.stage()));
+        }
+        logger::info(&a, &text).await;
+    });
 
     // Tasks
     tasks::init_bot_list_posting_loop(assyst.clone());
@@ -37,8 +89,8 @@ async fn main() -> anyhow::Result<()> {
     tasks::init_caching_gc_loop(assyst.clone());
     tasks::update_patrons(assyst.clone());
     tasks::init_healthcheck(assyst.clone());
-    //tasks::init_metrics_collect_loop(arced_cluster.clone(), assyst.clone())
-    //    .context("Failed to initialize metrics collect loop")?;
+    tasks::init_metrics_collect_loop(arced_cluster.clone(), assyst.clone())
+        .context("Failed to initialize metrics collect loop")?;
 
     // Bot list webhooks and metrics
     webserver_run(
@@ -68,26 +120,16 @@ async fn main() -> anyhow::Result<()> {
 
     assyst.initialize_blacklist().await?;
 
-    let mut listener = UnixListener::bind(EVENT_PIPE)?;
-
-    let (mut stream, _) = listener.accept().await?;
-
     // Event loop
-    loop {
-        let mut data: Vec<u8> = Vec::new();
+    while let Some((_, event)) = events.next().await {
         let assyst_clone = assyst.clone();
-        stream.read_to_end(&mut data).await?;
         tokio::spawn(async move {
-            let json = String::from_utf8_lossy(&data);
-            let de = GatewayEventDeserializer::from_json(&json).unwrap();
-            let mut json_de = serde_json::Deserializer::from_str(&json);
-            match de.deserialize(&mut json_de) {
-                Ok(x) => {
-                    handle_event(assyst_clone, x).await;
-                }
-                Err(_) => {}
-            };
+            handle_event(assyst_clone, event).await;
         });
         assyst.metrics.add_event();
     }
+
+    println!("{}", "shutting down");
+
+    Ok(())
 }
