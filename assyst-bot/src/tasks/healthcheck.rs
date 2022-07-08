5b5747176f85c694132f0293fd9e8c332fef14a4
Jacherr <jwc2002@outlook.com>
2022-07-08 22:18:48 +0100

    loop health

--- assyst-bot/src/tasks/healthcheck.rs
+++ assyst-bot/src/tasks/healthcheck.rs
@@ -12,19 +12,21 @@ use crate::{
 };
 
 pub fn init_healthcheck(assyst: Arc<Assyst>) {
-    tokio::spawn(async move {
-        loop {
-            let result = healthcheck(assyst.clone()).await;
-
-            for check in &result {
-                if check.status == ServiceStatus::Offline {
-                    logger::fatal(&assyst, &format!("{} is offline", check.service)).await;
+    loop {
+        let assyst = assyst.clone();
+        tokio::spawn(async move {
+            loop {
+                sleep(Duration::from_secs(5 * 60)).await;
+                let result = healthcheck(assyst.clone()).await;
+    
+                for check in &result {
+                    if check.status == ServiceStatus::Offline {
+                        logger::fatal(&assyst, &format!("{} is offline", check.service)).await;
+                    }
                 }
+    
+                *assyst.healthcheck_result.lock().await = (Instant::now(), result);
             }
-
-            *assyst.healthcheck_result.lock().await = (Instant::now(), result);
-
-            sleep(Duration::from_secs(5 * 60)).await;
-        }
-    });
+        });
+    }
 }
