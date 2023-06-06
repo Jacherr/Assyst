baf646077b9cef6e374f4b08a6c62abe99e7a19a
y21 <30553356+y21@users.noreply.github.com>
2023-06-06 16:56:47 +0200

    don't unwrap event in gateway

--- assyst-gateway/src/main.rs
+++ assyst-gateway/src/main.rs
@@ -1,6 +1,11 @@
 #![feature(never_type)]
 
-use std::{collections::HashMap, sync::Arc, time::{Duration, SystemTime}, rc::Rc};
+use std::{
+    collections::HashMap,
+    rc::Rc,
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
 
 use assyst_common::{
     config::Config,
@@ -8,22 +13,26 @@ use assyst_common::{
         gateway::{self, Latencies},
         EVENT_PIPE,
     },
-    ok_or_break
+    ok_or_break,
 };
 use bincode::serialize;
 use futures_util::StreamExt;
 use tokio::{
+    fs::remove_file,
     io::{AsyncWriteExt, BufWriter},
     net::UnixListener,
     sync::Mutex,
-    time::sleep, fs::remove_file,
+    time::sleep,
 };
-use twilight_gateway::{stream::{create_recommended, ShardMessageStream}, Intents, Config as GatewayConfig, Message};
+use twilight_gateway::{
+    stream::{create_recommended, ShardMessageStream},
+    Config as GatewayConfig, Intents, Message,
+};
+use twilight_http::Client;
 use twilight_model::gateway::{
     payload::outgoing::update_presence::UpdatePresencePayload,
     presence::{Activity, ActivityType, Status},
 };
-use twilight_http::Client;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<!> {
@@ -51,9 +60,18 @@ async fn main() -> anyhow::Result<!> {
     };
     let presence = UpdatePresencePayload::new(vec![activity], false, None, Status::Online)?;
     let http_client = Client::new(config.auth.discord.to_string());
-    let gateway_config = GatewayConfig::builder(config.auth.discord.to_string(), Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::MESSAGE_CONTENT).presence(presence).build();
+    let gateway_config = GatewayConfig::builder(
+        config.auth.discord.to_string(),
+        Intents::GUILD_MESSAGES | Intents::GUILDS | Intents::MESSAGE_CONTENT,
+    )
+    .presence(presence)
+    .build();
 
-    let mut shards = create_recommended(&http_client, gateway_config.clone(), |_, _| gateway_config.clone()).await?.collect::<Vec<_>>();
+    let mut shards = create_recommended(&http_client, gateway_config.clone(), |_, _| {
+        gateway_config.clone()
+    })
+    .await?
+    .collect::<Vec<_>>();
     let stream = Arc::new(Mutex::new(ShardMessageStream::new(shards.iter_mut())));
 
     let listener = UnixListener::bind(EVENT_PIPE)?;
@@ -66,7 +84,7 @@ async fn main() -> anyhow::Result<!> {
 
 pub async fn supply_connection(
     listener: Rc<UnixListener>,
-    gw_stream: Arc<Mutex<ShardMessageStream<'_>>>
+    gw_stream: Arc<Mutex<ShardMessageStream<'_>>>,
 ) -> anyhow::Result<()> {
     let (stream, _) = listener.accept().await?;
     let writer = Arc::new(Mutex::new(BufWriter::new(stream)));
@@ -97,8 +115,8 @@ pub async fn supply_connection(
 
     // Event loop
     while let Some((_, event)) = gw_stream.lock().await.next().await {
-        match event.unwrap() {
-            Message::Text(x) => {
+        match event {
+            Ok(Message::Text(x)) => {
                 let mut lock = writer.lock().await;
                 ok_or_break!(lock.write_u8(gateway::OP_EVENT).await);
                 ok_or_break!(lock.write_u32(x.as_bytes().len() as u32).await);
@@ -106,6 +124,7 @@ pub async fn supply_connection(
                 ok_or_break!(lock.flush().await);
             }
             o => {
+                // Message::Close event or error?
                 println!("{:?}", o);
             }
         }
